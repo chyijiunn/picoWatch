48a00ce4ea34c27e69f0c82d44fce8caed55c4b3
chyijiunn <chyijiunn@users.noreply.github.com>
2023-11-18 11:51:05 +0800

    Update 02_time.py

--- 02_time.py
+++ 02_time.py
@@ -3,7 +3,7 @@ import RP
 LCD = RP.LCD_1inch28()
 LCD.set_bl_pwm(65535)#設定亮度~65535
 
-LCD.fill_rect(0,0,240,40,LCD.red)#畫一個長方形，從0,0開始，大小為240 ,40
+LCD.fill_rect(0,0,240,40,LCD.red)
 LCD.text("Now",110,25,LCD.white)#寫一個字，位置是110,25,顏色是白色
 LCD.show()#讓上面的呈現出來
 

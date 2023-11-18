e75ca7ba5af61de80233ca9a2ce9e1da3bd0efd8
chyijiunn <chyijiunn@users.noreply.github.com>
2023-11-18 11:52:25 +0800

    Update 02_time.py

--- 02_time.py
+++ 02_time.py
@@ -5,7 +5,7 @@ LCD.set_bl_pwm(65535)#設定亮度~65535
 
 LCD.fill_rect(0,0,240,40,LCD.red)
 LCD.text("Now",110,25,LCD.white)
-LCD.show()#讓上面的呈現出來
+LCD.show()
 
 now = time.localtime()#讀取機器內時間
 LCD.text(str(now),0,120,LCD.black)#把剛剛時間寫入LCD

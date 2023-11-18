468d90413cd86cfc23c1dd79aa1947fd4389c468
chyijiunn <chyijiunn@users.noreply.github.com>
2023-11-18 10:57:45 +0800

    Update 05_wRefresh.py.py

--- 05_wRefresh.py.py
+++ 05_wRefresh.py.py
@@ -7,7 +7,7 @@ LCD.fill_rect(0,0,240,40,LCD.red)
 LCD.text("Now",110,25,LCD.white)
 LCD.show()
 while 1:
-    LCD.fill_rect(0,40,240,200,LCD.white)
+    LCD.fill_rect(0,40,240,200,LCD.white)#局部更新範圍
     now = list(time.localtime())
     LCD.text(str(now[3:6]),80,120,LCD.black)
     LCD.show()

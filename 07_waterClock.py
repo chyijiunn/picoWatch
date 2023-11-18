a41bf808b45d1a70333194ef9529f0f39146633e
chyijiunn <chyijiunn@users.noreply.github.com>
2023-11-18 11:07:10 +0800

    Update 07_waterClock.py

--- 07_waterClock.py
+++ 07_waterClock.py
@@ -10,9 +10,9 @@ while 1:
     LCD.fill_rect(0,0,240,236-(4*sec),LCD.white)
     LCD.fill_rect(0,236-(4*sec),240,240,0x180f)
     
-    LCD.text(str(now[3])+':'+str(int(now[4])),100,120,LCD.black)
+    LCD.text(str(now[3])+':'+str(int(now[4])),100,120,LCD.black)#加上分、冒號在秒數前
 
     LCD.show()
-    time.sleep(0.9)
+    time.sleep(0.9)#為何是 0.9 ?0.5 or 0.4 可行否？
 
 

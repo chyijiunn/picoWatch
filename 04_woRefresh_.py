e4c1d3bd8d08982bf5e6e197d913d7dbe7ccbb05
chyijiunn <chyijiunn@users.noreply.github.com>
2023-11-18 10:57:11 +0800

    Update 04_woRefresh_.py

--- 04_woRefresh_.py
+++ 04_woRefresh_.py
@@ -6,7 +6,7 @@ LCD.set_bl_pwm(65535)
 LCD.fill_rect(0,0,240,40,LCD.red)
 LCD.text("Now",110,25,LCD.white)
 LCD.show()
-while 1:#重複更新時間，並暫停一秒，這邊的停頓一秒是真的一秒嗎？
+while 1:#重複更新時間，並暫停一秒，這邊的停頓一秒是真的一秒嗎？還有一個問題
     now = list(time.localtime())
     LCD.text(str(now[3:6]),80,120,LCD.black)
     LCD.show()

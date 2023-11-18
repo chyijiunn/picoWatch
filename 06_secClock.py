e9323119b0d6e462f1d810fcc1364736fa02ec0e
chyijiunn <chyijiunn@users.noreply.github.com>
2023-11-18 11:04:08 +0800

    Update 06_secClock.py

--- 06_secClock.py
+++ 06_secClock.py
@@ -5,11 +5,11 @@ LCD.set_bl_pwm(65535)
 
 while 1:
     now = list(time.localtime())
-    sec = now[5]
-    LCD.fill_rect(0,0,240,236-(4*sec),LCD.white)
-    LCD.fill_rect(0,236-(4*sec),240,4,0x180f)
-    LCD.fill_rect(80,120,80,10,LCD.white)
-    LCD.text(str(sec),110,120,LCD.black)
+    sec = now[5]#讀取系統秒數
+    LCD.fill_rect(0,0,240,236-(4*sec),LCD.white)#更動時，上方白色往上拉
+    LCD.fill_rect(0,236-(4*sec),240,4,0x180f)#更動時，下方綠色往上拉
+    LCD.fill_rect(80,120,80,10,LCD.white)#維持數字更新
+    LCD.text(str(sec),110,120,LCD.black)#最後疊上秒數
 
     LCD.show()
     time.sleep(1)

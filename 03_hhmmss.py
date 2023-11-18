4f1be1c2ddcc0d6716e46ceab7d5f199fecfccc4
chyijiunn <chyijiunn@users.noreply.github.com>
2023-11-18 10:54:08 +0800

    Update 03_hhmmss.py

--- 03_hhmmss.py
+++ 03_hhmmss.py
@@ -3,11 +3,11 @@ import RP
 LCD = RP.LCD_1inch28()
 LCD.set_bl_pwm(65535)
 
-LCD.fill_rect(0,0,240,40,LCD.black)#從 (0,0) 增加長寬 (240,40)
+LCD.fill_rect(0,0,240,40,LCD.black)
 LCD.text("Now",110,25,LCD.white)
 
 now = list(time.gmtime())
-LCD.text(str(now[3:6]),80,120,LCD.cyan)#list 3 ~ 6 ，不含 6
+LCD.text(str(now[3:6]),80,120,LCD.cyan)#list格式 3 ~ 6 ，不含 6
 LCD.show()
 
-print(now)
+print(now)#電腦螢幕上的是否相同

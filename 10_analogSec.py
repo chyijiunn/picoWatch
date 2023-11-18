5250b8a3f9fcee7e7d96258c1aee41e6666110ff
chyijiunn <chyijiunn@users.noreply.github.com>
2023-11-18 11:19:06 +0800

    Update 10_analogSec.py

--- 10_analogSec.py
+++ 10_analogSec.py
@@ -6,20 +6,21 @@ LCD.set_bl_pwm(65535)
 while 1:
     LCD.fill_rect(0,0,240,240,LCD.white)
     now = list(time.localtime())
-    #second
-    sec = now[5] * 6#sec/60*360
+    #秒針
+    sec = now[5] * 6
     radian = math.radians(sec)
     x_sin = math.sin(radian)
     y_cos = math.cos(radian)
     LCD.line(120,120,int(120*x_sin+120),int(-120*y_cos+120),LCD.red)
     
-    #minute
-    minute = now[4] * 6#min/60*360
+    #分針
+    minute = now[4] * 6
     radian = math.radians(minute)
     x_sin = math.sin(radian)
     y_cos = math.cos(radian)
     LCD.line(120,120,int(110*x_sin+110),int(-110*y_cos+110),LCD.black)
     
+    #時針的設計一樣嗎？
     LCD.show()
     time.sleep(1)
     
